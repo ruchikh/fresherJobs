b88f7530ccea0fb1b527290bc3745bbdc002a2c4
Ruchi <ruchikharwar4@gmail.com>
2019-04-13 14:50:58 +0530

    added jobDetails component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import JobDetails from './components/JobDetails'
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        homepage
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={JobDetails} />
+
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- client/src/components/JobDetails.js
+++ client/src/components/JobDetails.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
 class JobDetails extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			title: "",
+			role: "",
+			description: "",
+			techStack: "",
+			experience: "",
+			location: "",
+			aboutCompany: ""
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	handleSubmit = (e) => {
+
+	}
+
   render() {
     return (
       <div className="jobs">
-        
+       	<from>
+       		<label for="male">Titile</label>
+       		<input type="text" name="title" placeholder="Enter Titile" onChange={this.handleChange}/>
+       		<label for="male">Role</label>
+       		<input type="text" name="role" placeholder="Role" onChange={this.handleChange}/>
+       		<label for="male">Location</label>
+       		<input type="text" name="location" placeholder="location" onChange={this.handleChange}/>
+       		<label for="male">Skills</label>
+       		<input type="text" name="techStack" placeholder="Skills" onChange={this.handleChange}/>
+       		<label for="male">Experience</label>
+       		<input type="text" name="experience" placeholder="Experience" onChange={this.handleChange}/>
+       		<label for="male">Job Description</label>
+       		<textarea name="description" placeholder="descrption" onChange={this.handleChange}/>
+       		<label for="male">About Company</label>
+       		<textarea name ="aboutCompany" placeholder="about company" onChange={this.handleChange}/>
+       	</from>
       </div>
     );
   }
